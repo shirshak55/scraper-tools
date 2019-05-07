390e3c219607d34ebf66448ca9385bf2b7028499
shirshak <8097377+shirshak55@users.noreply.github.com>
2019-05-07 12:40:49 +0545

    add chalk

--- index.ts
+++ index.ts
@@ -14,6 +14,7 @@ import request from './src/request'
 import xlsxReader from './src/xlsxReader'
 import fastPage from './src/fastPage'
 import delay from 'delay'
+import chalk from 'chalk'
 
 export {
     chunks,
@@ -28,6 +29,7 @@ export {
     inquirer,
     loadDom,
     meow,
+    chalk,
     randomNumberRange,
     readFileSync,
     request,
