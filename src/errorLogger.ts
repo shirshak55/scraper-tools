600c29734c06baf76713bc0c1bf31fc9223e2de4
shirshak <8097377+shirshak55@users.noreply.github.com>
2019-05-17 21:16:18 +0545

    add inquirer types

--- src/errorLogger.ts
+++ src/errorLogger.ts
@@ -1,10 +1,11 @@
-import fs from 'fs'
+import fs from 'fs-extra'
 import util from 'util'
+import consoleMessage from './consoleMessage'
 
-export default (err) => {
-    console.log('[main] We got error')
-    console.error(err)
-    var log_file_err = fs.createWriteStream(__dirname + '../../error.log', { flags: 'a' })
+export default (err, path) => {
+    consoleMessage.error('Error Logger', 'We Got Error', err)
+    fs.ensureFileSync(path)
+    var log_file_err = fs.createWriteStream(path, { flags: 'a' })
     log_file_err.write(util.format('Caught exception: ' + err) + '\n')
     process.exit(1)
 }
