f8a35904df7611ba4d6e90545a6d7309e10c8b24
shirshak <8097377+shirshak55@users.noreply.github.com>
2019-05-25 16:19:16 +0545

    improve logger

--- src/consoleMessage.ts
+++ src/consoleMessage.ts
@@ -6,57 +6,62 @@ import os from 'os'
 import rightPad from 'right-pad'
 
 export default (() => {
-    let desktopPath = path.join(os.homedir(), 'Desktop')
-    let logFolder = createDirectories(path.join(desktopPath, 'logs'))
+  let shouldLogToFile = false
+  let desktopPath = path.join(os.homedir(), 'Desktop')
+  let logFolder = createDirectories(path.join(desktopPath, 'logs'))
 
-    const setLogFolder = (value: string) => {
-        logFolder = value
-    }
+  const setLogFolder = (value: string) => {
+    logFolder = value
+  }
 
-    const titlify = (title: string | { title: string; padding: number }) => {
-        if (typeof title === 'object') {
-            return rightPad(`[${title.title}]`, title.padding, ' ')
-        } else {
-            return `[${title}]`
-        }
+  const titlify = (title: string | { title: string; padding: number }) => {
+    if (typeof title === 'object') {
+      return rightPad(`[${title.title}]`, title.padding, ' ')
+    } else {
+      return `[${title}]`
     }
+  }
 
-    const logToFile = (title, content) => {
-        let c = content
+  const logToFile = (title, content) => {
+    if (!shouldLogToFile) return
+    let c = content
 
-        try {
-            let c = JSON.stringify(titlify(title) + ' ' + content, null, 4)
-        } catch (e) {}
+    try {
+      let c = JSON.stringify(titlify(title) + ' ' + content, null, 4)
+    } catch (e) {}
 
-        fs.appendFileSync(path.join(logFolder, 'log.txt'), '\n' + c)
-    }
+    fs.appendFileSync(path.join(logFolder, 'log.txt'), '\n' + c)
+  }
 
-    const error = (title, ...s) => {
-        logToFile(title, s.join(' '))
-        return console.log(chalk.bgRed.green(titlify(title)), ...s)
-    }
-    const info = (title, ...s) => {
-        logToFile(title, s.join(' '))
-        console.log(chalk.bgBlue.white(titlify(title)), ...s)
-    }
-    const warning = (title, ...s) => {
-        logToFile(title, s.join(' '))
-        console.log(chalk.bgYellow.red(titlify(title)), ...s)
-    }
+  const error = (title, ...s) => {
+    logToFile(title, s.join(' '))
+    return console.log(chalk.bgRed.green(titlify(title)), ...s)
+  }
+  const info = (title, ...s) => {
+    logToFile(title, s.join(' '))
+    console.log(chalk.bgBlue.white(titlify(title)), ...s)
+  }
+  const warning = (title, ...s) => {
+    logToFile(title, s.join(' '))
+    console.log(chalk.bgYellow.red(titlify(title)), ...s)
+  }
 
-    const success = (title, ...s) => {
-        logToFile(title, s.join(' '))
-        console.log(chalk.bgGreen.red(titlify(title)), ...s)
-    }
+  const success = (title, ...s) => {
+    logToFile(title, s.join(' '))
+    console.log(chalk.bgGreen.red(titlify(title)), ...s)
+  }
 
-    return {
-        desktopPath,
-        logFolder,
-        setLogFolder,
-        logToFile,
-        error,
-        info,
-        warning,
-        success,
-    }
+  return {
+    desktopPath,
+    logFolder,
+    setLogFolder,
+    logToFile,
+    error,
+    info,
+    warning,
+    success,
+    setShouldLogToFile: (value: boolean = false) => {
+      shouldLogToFile: value
+    },
+  }
 })()
--- src/errorLogger.ts
+++ src/errorLogger.ts
@@ -3,9 +3,9 @@ import util from 'util'
 import consoleMessage from './consoleMessage'
 
 export default (err, path) => {
-    consoleMessage.error('Error Logger', 'We Got Error', err)
-    fs.ensureFileSync(path)
-    var log_file_err = fs.createWriteStream(path, { flags: 'a' })
-    log_file_err.write(util.format('Caught exception: ' + err) + '\n')
-    process.exit(1)
+  consoleMessage.error('Error Logger', 'We Got Error', err)
+  fs.ensureFileSync(path)
+  let log_file_err = fs.createWriteStream(path, { flags: 'a' })
+  log_file_err.write(util.format('Caught exception: ' + err) + '\n')
+  process.exit(1)
 }
