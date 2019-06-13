2e20e7adbfc96e8a6b3853a768f7e9bff846e400
shirshak <8097377+shirshak55@users.noreply.github.com>
2019-06-13 23:46:57 +0545

    v1.0.84

---
+++ dist/src/sftp.js
@@ -0,0 +1,44 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ssh2_sftp_client_1 = __importDefault(require("ssh2-sftp-client"));
+const consoleMessage_1 = __importDefault(require("./consoleMessage"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+/// # Allows you to upload Via SSH
+/// @Todo Lock
+exports.default = (() => {
+    let sftpHandler;
+    return {
+        upload: async ({ host, port, username, privatekeyPath, filePath, remoteFilePath, }) => {
+            try {
+                let sftp = new ssh2_sftp_client_1.default();
+                await sftp.connect({
+                    host,
+                    port,
+                    username,
+                    privateKey: fs_1.default.readFileSync(privatekeyPath),
+                });
+                let sftpHandler = sftp;
+                try {
+                    await sftp.mkdir(path_1.default.dirname(remoteFilePath), true);
+                }
+                catch (e) { }
+                await sftp.put(filePath, remoteFilePath);
+                await sftp.end();
+            }
+            catch (e) {
+                consoleMessage_1.default.error('SSH Uploader Module', e);
+            }
+        },
+        handler: async () => {
+            if (!sftpHandler) {
+                throw 'No SSH Handler found';
+            }
+            return sftpHandler;
+        },
+    };
+})();
+//# sourceMappingURL=sftp.js.map
---
+++ dist/src/ssh.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const consoleMessage_1 = __importDefault(require("./consoleMessage"));
+const node_ssh_1 = __importDefault(require("node-ssh"));
+/// # Allows you to upload Via SSH
+/// @Todo Lock
+exports.default = (() => {
+    let handler;
+    return {
+        handler: async () => {
+            if (!handler) {
+                throw 'No SSH Handler found';
+            }
+            return handler;
+        },
+        connect: async ({ host, port, username, privatekeyPath, }) => {
+            try {
+                let ssh = new node_ssh_1.default();
+                handler = await ssh.connect({
+                    host,
+                    port,
+                    username,
+                    privateKey: privatekeyPath,
+                });
+            }
+            catch (e) {
+                consoleMessage_1.default.error('Error at ssh', e);
+            }
+        },
+    };
+})();
+//# sourceMappingURL=ssh.js.map
