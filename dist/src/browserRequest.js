26637eda05b5b972bb8e2a681329deeba208c9b3
Shirshak <8097377+shirshak55@users.noreply.github.com>
2019-10-09 11:37:42 +0545

    Fix browser request module

--- dist/src/browserRequest.js
+++ dist/src/browserRequest.js
@@ -1,25 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-async function browserRequest(page, url, method = 'GET') {
+async function browserRequest(page, config = {}) {
+    if (!config.method) {
+        config.method = 'GET';
+    }
+    if (!config.url) {
+        throw 'URL is not given. Please provide Url';
+    }
     let evaluated = await page.evaluate(async (url, method) => {
         console.log('start--------------------------------------------------------------------------');
         console.log('Sending Request to url', url, method);
-        let res = await fetch(url, {
+        let res = await fetch(config.url, {
             credentials: 'include',
             headers: {
                 accept: '*/*',
                 'accept-language': 'en-US,en;q=0.9'
             },
             body: null,
-            method,
+            method: config.method,
             redirect: 'follow',
-            mode: 'cors'
+            mode: 'cors',
+            ...config
         });
         let toRet = res.text();
         console.log(toRet);
         console.log('end--------------------------------------------------------------------------');
         return toRet;
-    }, url, method);
+    }, config);
     return evaluated;
 }
 exports.default = browserRequest;
--- src/browserRequest.ts
+++ src/browserRequest.ts
@@ -1,29 +1,34 @@
 import { Page } from 'puppeteer'
 
-export default async function browserRequest(page: Page, url, method = 'GET') {
-  let evaluated = await page.evaluate(
-    async (url, method) => {
-      console.log('start--------------------------------------------------------------------------')
-      console.log('Sending Request to url', url, method)
+export default async function browserRequest(page: Page, config: any = {}) {
+  if (!config.method) {
+    config.method = 'GET'
+  }
 
-      let res = await fetch(url, {
-        credentials: 'include',
-        headers: {
-          accept: '*/*',
-          'accept-language': 'en-US,en;q=0.9'
-        },
-        body: null,
-        method,
-        redirect: 'follow',
-        mode: 'cors'
-      })
-      let toRet = res.text()
-      console.log(toRet)
-      console.log('end--------------------------------------------------------------------------')
-      return toRet
-    },
-    url,
-    method
-  )
+  if (!config.url) {
+    throw 'URL is not given. Please provide Url'
+  }
+
+  let evaluated = await page.evaluate(async (url, method) => {
+    console.log('start--------------------------------------------------------------------------')
+    console.log('Sending Request to url', url, method)
+
+    let res = await fetch(config.url, {
+      credentials: 'include',
+      headers: {
+        accept: '*/*',
+        'accept-language': 'en-US,en;q=0.9'
+      },
+      body: null,
+      method: config.method,
+      redirect: 'follow',
+      mode: 'cors',
+      ...config
+    })
+    let toRet = res.text()
+    console.log(toRet)
+    console.log('end--------------------------------------------------------------------------')
+    return toRet
+  }, config)
   return evaluated
 }
