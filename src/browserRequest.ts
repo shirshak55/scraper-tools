118ed8baa2a33b8dd9f85d1b5a2cada995edbbdb
Shirshak <8097377+shirshak55@users.noreply.github.com>
2020-08-04 12:34:23 +0545

    add hook support for browser request

--- src/browserRequest.ts
+++ src/browserRequest.ts
@@ -4,7 +4,13 @@ import AsyncLock from "async-lock"
 
 let lock = new AsyncLock({ maxPending: 5000 })
 
+let hooks: Array<(page: Page, config: any) => any> = []
+
 export async function browserRequest(page: Page, config: any = {}) {
+  for (let hook of hooks) {
+    await hook(page, config)
+  }
+
   if (!config.url) {
     throw "URL is not given. Please provide Url"
   }
@@ -29,6 +35,11 @@ export async function browserRequest(page: Page, config: any = {}) {
     return toRet
   }, fetchConfig)
   return evaluated
+}
+
+// Async just in case in future we need to add some await here
+export async function addBrowserRequestHooks(func: (page: Page, config: any) => any) {
+  hooks.push(func)
 }
 
 export async function singleBrowserRequest(page: Page, config: any = {}) {
